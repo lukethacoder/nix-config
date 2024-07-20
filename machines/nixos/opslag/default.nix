9046978a21981cb9cf5fd38e906809e2d9be9954
Luke Secomb <luke.secomb@live.com>
2024-07-20 11:59:13 +1000

    🔒 fix displayManager config / add flake.lock file

--- machines/nixos/opslag/default.nix
+++ machines/nixos/opslag/default.nix
@@ -2,8 +2,6 @@
 {
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  services.xserver.displayManager.gdm.autoSuspend = false;
-
   # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -33,12 +31,15 @@
   # Configure keymap in X11
   services.xserver = {
     enable = true;
-    displayManager.gdm.enable = true;
-    desktopManager.gnome.enable = true;
     xkb = {
       layout = "au";
     };
     displayManager = {
+      gdm = {
+        enable = true;
+        autoSuspend = false;
+      };
+      gnome.enable = true;
       setupCommands = ''
         ${pkgs.xorg.xrandr}/bin/xrandr --output DP-1 --off DP-2 --off --output HDMI-1 --mode 1920x1080 --pos 0x0 --rota>      '';
     };
