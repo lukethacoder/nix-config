871ab6208179950a92efc6bab649d3b8792cd74b
lukethacoder <13529535+lukethacoder@users.noreply.github.com>
2025-01-11 22:46:37 +1100

    🎧  add second navidrome instance specifically for mixes

--- containers/navidrome/default.nix
+++ containers/navidrome/default.nix
@@ -1,6 +1,7 @@
 { config, vars, pkgs, ... }:
 let directories = [
   "${vars.serviceConfigRoot}/navidrome"
+  "${vars.serviceConfigRoot}/navidrome-mixes"
   "${vars.mainArray}/Media/Music"
 ];
 in {
@@ -8,7 +9,9 @@ in {
 
   system.userActivationScripts.navidrome-data.text = ''
     mkdir -p ${vars.serviceConfigRoot}/navidrome \
-      ${vars.mainArray}/Media/Music/Music
+      ${vars.mainArray}/Media/Music/Music \
+      ${vars.serviceConfigRoot}/navidrome-mixes \
+      ${vars.mainArray}/Media/Music/Mixes
   '';
 
   virtualisation.oci-containers = {
@@ -47,6 +50,43 @@ in {
           "-l=homepage.widget.token={{HOMEPAGE_FILE_NAVIDROME_TOKEN}}"
           "-l=homepage.widget.salt={{HOMEPAGE_FILE_NAVIDROME_SALT}}"
           "-l=homepage.widget.url=https://navidrome.${builtins.readFile config.sops.secrets.domain_name.path}"
+        ];
+      };
+      # Separate instance purely for DJ Mixes
+      navidrome_mixes = {
+        image = "deluan/navidrome:latest";
+        autoStart = true;
+        ports = [ "4534:4533" ];
+        volumes = [
+          "${vars.serviceConfigRoot}/navidrome-mixes:/data"
+          "${vars.mainArray}/Media/Music/Mixes:/music:ro"
+        ];
+        environment = {
+          TZ = config.sops.secrets.time_zone.path;
+          PUID = "1000";
+          PGID = "1000";
+          ND_SCANSCHEDULE = "1h";
+          ND_LOGLEVEL = "info";
+          ND_SESSIONTIMEOUT = "24h";
+          ND_BASEURL = "http://mixes.${builtins.readFile config.sops.secrets.domain_name.path}";
+          ND_LASTFM_ENABLED = "true";
+          ND_LASTFM_APIKEY = builtins.readFile config.sops.secrets."lastfm/api_key".path;
+          ND_LASTFM_SECRET = builtins.readFile config.sops.secrets."lastfm/api_secret".path;
+        };
+        extraOptions = [
+          "-l=traefik.enable=true"
+          "-l=traefik.http.routers.navidromemixes.rule=Host(`mixes.${builtins.readFile config.sops.secrets.domain_name.path}`)"
+          "-l=traefik.http.services.navidromemixes.loadbalancer.server.port=4533"
+          "-l=homepage.group=Media"
+          "-l=homepage.name=Navidrome Mixes"
+          "-l=homepage.icon=https://simpleicons.org/icons/pioneerdj.svg"
+          "-l=homepage.href=https://mixes.${builtins.readFile config.sops.secrets.domain_name.path}"
+          "-l=homepage.description=Media player"
+          "-l=homepage.widget.type=navidrome"
+          "-l=homepage.widget.user={{HOMEPAGE_FILE_NAVIDROME_USERNAME}}"
+          "-l=homepage.widget.token={{HOMEPAGE_FILE_NAVIDROME_TOKEN}}"
+          "-l=homepage.widget.salt={{HOMEPAGE_FILE_NAVIDROME_SALT}}"
+          "-l=homepage.widget.url=https://mixes.${builtins.readFile config.sops.secrets.domain_name.path}"
         ];
       };
     };
