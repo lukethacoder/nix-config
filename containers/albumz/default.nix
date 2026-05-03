50b2e5ddde3cd2f27a70987f144547aca3c26be9
lukethacoder <13529535+lukethacoder@users.noreply.github.com>
2026-05-03 19:25:02 +1000

    🤫  add proper albumz secrets

--- containers/albumz/default.nix
+++ containers/albumz/default.nix
@@ -7,11 +7,7 @@ let
   rootDir = "${vars.serviceConfigRoot}/albumz";
 
   dbName = "albumz";
-  dbHostName = "albumz_db";
   dbDir = "${rootDir}/db";
-
-  postgresUser = "admin";
-  postgresPassword = "password";
 
   directories = [
     rootDir
@@ -31,16 +27,11 @@ in {
           ];
           environment = {
             TZ = vars.timeZone;
-            DATABASE_URL = "postgresql://${postgresUser}:${postgresPassword}@${dbHostName}:5432/${dbName}";
-            JWT_SECRET = "test123";
             ORIGIN = "https://albumz.${vars.domainName}";
-            # LASTFM_API_KEY
-            # SPOTIFY_CLIENT_ID
-            # SPOTIFY_CLIENT_SECRET
           };
-          # environmentFiles = [
-          #   config.sops.templates."albumz-env".path
-          # ];
+          environmentFiles = [
+            config.sops.templates."albumz-env".path
+          ];
           extraOptions = [
             "-l=traefik.enable=true"
             "-l=traefik.http.routers.albumz.rule=Host(`albumz.${vars.domainName}`)"
@@ -66,14 +57,12 @@ in {
           volumes = [
             "${dbDir}:/var/lib/postgresql/data"
           ];
-          # environmentFiles = [
-          #   config.sops.templates."albumz-env".path
-          # ];
+          environmentFiles = [
+            config.sops.templates."albumz-env".path
+          ];
           environment = {
             DB_DATA_LOCATION = "/var/lib/postgresql/data";
             POSTGRES_DB = dbName;
-            POSTGRES_USER = postgresUser;
-            POSTGRES_PASSWORD = postgresPassword;
           };
         };
       };
